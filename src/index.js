44431929826c21ef380bf62bdd0fdcc5840764b3
halezmarie <teacherhaleymarie@gmail.com>
2021-08-11 15:42:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Selecting the form
+
+
+const taskForm = document.getElementById("create-task-form");
+
+taskForm.addEventListener("submit", handleSubmit);
+// listen for submit of the form 
+
+// what to do with the form when it is submitted 
+function handleSubmit(event){
+  event.preventDefault(); 
+  console.log('the event value is:', event.value);
+  const newTask = document.getElementById("new-task-description").value;
+  // create elements with the form data
+  // add those elements inside the list div
+  tasks.innerHTML +=`
+  <li>
+  ${newTask}
+  </li>` 
+
+taskForm.reset(); // clears the values of the form elements for a better user experience 
+}
 });
